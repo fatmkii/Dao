d6a26e76fd73199746ef3d262927a9717e59813c
fatmkii <47155837@qq.com>
2022-07-14 22:16:52 +0800

    每日重启Echo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->command('BattlePolling:run')->everyMinute();
         $schedule->command('DelayThreadHandle:run')->dailyAt('8:00');
+        $schedule->exec('supervisorctl restart Laravel-Echo-server:Laravel-Echo-server_00')->dailyAt('3:00');
     }
 
     /**
