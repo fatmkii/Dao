78684ffb3236c0dafc10b78e9c07feaeb2270eb0
blueberry <47155837@qq.com>
2022-09-11 23:51:31 +0800

    调整

--- app/Common/BattleChara.php
+++ app/Common/BattleChara.php
@@ -562,7 +562,7 @@ class BattleChara
         switch ($chara_id) {
             case 10: //元元可以掷出最大101点
                 return random_int(1, 101);
-            case 3: 
+            case 6: 
                 return random_int(80, 100);                     
             default:
                 return random_int(1, 100);
