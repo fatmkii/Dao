86170c9d19ac6b960b08f5391cdfa0f01d9ccf36
fatmkii <47155837@qq.com>
2023-12-14 21:18:52 +0800

    修正

--- database/migrations/2023_09_13_212556_create_user_bank_table.php
+++ database/migrations/2023_09_13_212556_create_user_bank_table.php
@@ -19,7 +19,7 @@ class CreateUserBankTable extends Migration
             $table->unsignedInteger('olo');
             $table->string('description')->nullable();
             $table->boolean('is_deleted')->default(0)->index();
-            $table->timestamp('expire_date');
+            $table->timestamp('expire_date')->nullable(); //在阿里云rds上，不知道为什么not null的话一定会default current_timestamp
             $table->timestamp('withdraw_date')->nullable();
             $table->timestamps();
         });
